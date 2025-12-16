34ad311d477eab230ed3084f92864a30bd8a81bd
Farr Foxr <farr.foxr@gmail.com>
2025-12-16 11:31:54 +0700

    setting it up for deployment, need env key but from vercel website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,8 @@ export default function LiveCamera() {
             formData.append("file", blob, "frame.jpg");
 
             try {
-              const response = await fetch("http://localhost:8000/predict", {
+              const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+              const response = await fetch(`${API_URL}/predict`, {
                 method: "POST",
                 body: formData,
               });
